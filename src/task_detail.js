2e48939588896f6c727b1240fa1d2e269e7bb33b
Jack Case <jackacase@gmail.com>
2024-01-21 13:03:42 -0500

    working on task detail page

--- src/task_detail.js
+++ src/task_detail.js
@@ -1,3 +1,9 @@
 const qr_code = document.getElementById('qr-code')
+const create_task_button = document.getElementById('create-task-button')
+const task_modal = document.getElementById('task-modal')
+
+create_task_button.onclick = () => {
+    task_modal.classList.add('is-active')
+}
 
 qr_code.setAttribute('value', window.location)
